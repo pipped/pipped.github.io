80a123e9441e7d1465aee8eb683107828377e284
Dylan Sokolov <Dylan@Mariannas-MacBook-Air.local>
2026-04-20 20:01:49 -0700

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',   // 👈 ADD THIS LINE
   plugins: [react(), tailwindcss()],
 })
